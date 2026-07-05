269019b0c2b14b684e1fe65f53db73cfdc2632ca
¨david <idowuasabidavid@gmail>
2026-07-05 11:47:44 +0200

    Route visa.nextstepc.com to the Visa homepage

--- apps/nextstep/views.py
+++ apps/nextstep/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import NextStepFAQ, NextStepService, NextStepTestimonial
 
 def home(request):
+    if 'visa.nextstepc.com' in request.get_host():
+        from apps.pages.views import home as visa_home
+        return visa_home(request)
+
     context = {
         'services': NextStepService.objects.filter(is_active=True).order_by('order'),
         'testimonials': NextStepTestimonial.objects.filter(is_active=True),
